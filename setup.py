56d29e60154354dac4d350c50333ca3f564b7a82
jijingyuan <jijingyuan@rookiequant.com>
2024-09-25 22:10:35 +0800

    fix

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
     description="utils for algo modules",  # 包的简短描述
     packages=find_packages(),  # 自动发现包目录
     install_requires=[
-        "influxdb-client==1.46.0"
+        "influxdb-client==1.46.0",
+        'aiocsv',
+        'aiohttp'
     ],
 )

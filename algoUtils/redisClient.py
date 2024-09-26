3949706acf9a93d65d1210d4810ad841e037e419
jijingyuan <jijingyuan@rookiequant.com>
2024-09-26 17:50:42 +0800

    update

--- algoUtils/redisClient.py
+++ algoUtils/redisClient.py
@@ -4,6 +4,8 @@
 @File: redisClient.py
 @Author: Jingyuan
 """
+import time
+
 import redis
 from algoUtils.loggerUtil import generate_logger
 
@@ -13,6 +15,16 @@ logger = generate_logger(level='INFO')
 class RedisClient:
     def __init__(self, _host, _port):
         self.client = redis.Redis(_host, _port)
+
+    def flush_db(self, _db) -> bool:
+        try:
+            self.client.select(_db)
+            self.client.flushdb()
+            return True
+
+        except Exception as e:
+            logger.error(e)
+            return False
 
     def get_db_keys(self, _db) -> list or None:
         try:
@@ -87,18 +99,46 @@ class RedisClient:
             logger.error(e)
             return
 
-    def add_ts_batch(self, _db, _key, _batch: list):
+    def create_ts_key(self, _db, _key, _labels=None, _duplicate_policy='last'):
         try:
             self.client.select(_db)
             ts = self.client.ts()
-            with ts.pipeline() as pipe:
-                pipe.multi()
-                for data in _batch:
-                    pipe.ts().add(_key, data[0], data[1])
-                pipe.execute()
-
+            ts.create(_key, labels=_labels, duplicate_policy=_duplicate_policy)
             return True
 
         except Exception as e:
             logger.error(e)
             return False
+
+    def add_ts_point(self, _db, _key, _timestamp, _value):
+        try:
+            self.client.select(_db)
+            ts = self.client.ts()
+            ts.add(_key, _timestamp, _value)
+            return True
+
+        except Exception as e:
+            logger.error(e)
+            return False
+
+    def add_ts_batch(self, _db, _batch: list):
+        try:
+            self.client.select(_db)
+            ts = self.client.ts()
+            rsp = ts.madd(_batch)
+            check = [int(not isinstance(v, int)) for v in rsp]
+            return False if sum(check) > 0 else True
+
+        except Exception as e:
+            logger.error(e)
+            return False
+
+
+if __name__ == '__main__':
+    client = RedisClient('localhost', 7001)
+    # create ts key
+    # client.create_ts_key(10, 'test')
+
+    batch = [('test', int(time.time() * 1000000), 1), ('test', int(time.time() * 1000000 + 1), 2)]
+    print(client.add_ts_batch(10, batch))
+    # print(client.add_ts_point(10, 'test', int(time.time() * 1000), 111))

f4bc5c657c8d467fd29e5d6c2a58921f87b374f7
jijingyuan <jijingyuan@rookiequant.com>
2024-10-16 16:12:58 +0800

    update

--- algoUtils/DefUtil.py
+++ algoUtils/DefUtil.py
@@ -7,7 +7,7 @@
 import abc
 import uuid
 from typing import Optional, List, Dict
-import importlib
+from queue import PriorityQueue
 
 
 class SignalBase:
@@ -22,7 +22,6 @@ class SignalBase:
             'symbol': 'btc_usdt|binance_future',
             'action': 'open' or 'close'
             'position': 'long' or 'short'
-            'type': 'start' or 'end' or 'isolated' or 'consecutive',
             **kwargs: other info that you need for analyze
         }, ...]
         """
@@ -53,38 +52,34 @@ class OrderBase:
         'filled': 5
     }
 
-    def __init__(self, _method_name, _method_param):
-        self.exec_mgr = self.__get_exec_method(_method_name, _method_param)
+    def __init__(self, _exec_dict):
+        self.exec_dict = _exec_dict
         self.current_timestamp = None
-        self.__orders = {}
-        self.__sniffers = {}
-        self.__status_info = {}
-
-    @staticmethod
-    def __get_exec_method(_method_name, _method_param):
-        if _method_param is None:
-            _method_param = {}
-        module = importlib.import_module('algoExecution.algoExec.{}'.format(_method_name.lower()))
-        cls_method = getattr(module, _method_name)
-        if cls_method is None:
-            raise Exception('Unknown Method: {}'.format(_method_name))
-        instance = cls_method(**_method_param)
-        return instance
+        self._orders = {}
+        self._sniffers = {}
+        self._status_info = {}
 
     def update_current_timestamp(self, _timestamp):
         self.current_timestamp = _timestamp
 
-    def __update_send_timestamp(self, _order_id, _send_timestamp=None, _receive_timestamp=None):
+    def get_order_info(self, _order_id):
+        return self._orders.get(_order_id)
+
+    def get_sniffer_info(self, _order_id):
+        return self._sniffers.get(_order_id)
+
+    def _update_send_timestamp(self, _order_id, _send_timestamp=None, _receive_timestamp=None):
         if _send_timestamp is not None:
-            self.__orders[_order_id]['send_timestamp'] = _send_timestamp
+            self._orders[_order_id]['send_timestamp'] = _send_timestamp
 
         if _receive_timestamp is not None:
-            self.__orders[_order_id]['receive_timestamp'] = _receive_timestamp
+            self._orders[_order_id]['receive_timestamp'] = _receive_timestamp
 
-    def __generate_sniffer(
+    def _generate_sniffer(
             self,
             _bind_id,
             _symbol,
+            _exchange,
             _target_price,
             _operator,
             _order_type,
@@ -93,10 +88,12 @@ class OrderBase:
     ):
 
         order_id = str(uuid.uuid4())
-        self.__sniffers[order_id] = {
+        _, exchange = _symbol.split('|')
+        self._sniffers[order_id] = {
             'bind_id': _bind_id,
             'order_id': order_id,
             'symbol': _symbol,
+            'exchange': _exchange,
             'target_price': _target_price,
             'operator': _operator,
             'order_type': _order_type,
@@ -106,38 +103,40 @@ class OrderBase:
 
         return order_id
 
-    def __generate_order(
+    @abc.abstractmethod
+    def update_events(self, _event_q: PriorityQueue):
+        pass
+
+    def _generate_order(
             self,
             _bind_id,
             _symbol,
+            _exchange,
             _order_type,
             _action,
             _position,
             _feature=None,
             _expire=None,
             _delay=None,
-            _target_price=None,
-            _operator=None,
-            _condition_expire=None,
+            _condition=None,
             _price=None,
             _amount=None,
             _client_id=None,
     ):
         order_id = str(uuid.uuid4())
-        self.__orders[order_id] = {
+        self._orders[order_id] = {
             'bind_id': _bind_id,
             'order_id': order_id,
             'client_id': _client_id,
             'symbol': _symbol,
+            'exchange': _exchange,
             'order_type': _order_type,
             'action': _action,
             'position': _position,
             'feature': _feature,
             'expire': _expire,
             'delay': _delay,
-            'target_price': _target_price,
-            'operator': _operator,
-            'condition_expire': _condition_expire,
+            'condition': _condition,
             'price': _price,
             'amount': _amount,
             'current_timestamp': self.current_timestamp
@@ -145,9 +144,10 @@ class OrderBase:
         return order_id
 
     @abc.abstractmethod
-    def __update_status_info(
+    def update_status_info(
             self,
             _order_id,
+            _order_type,
             _status,
             _last_timestamp,
             _local_timestamp,
@@ -157,7 +157,7 @@ class OrderBase:
         pass
 
     @abc.abstractmethod
-    def create_order(
+    def place_order(
             self,
             _bind_id,
             _symbol,
@@ -167,16 +167,14 @@ class OrderBase:
             _feature=None,
             _expire=None,
             _delay=None,
-            _target_price=None,
-            _operator=None,
-            _condition_expire=None,
+            _condition=None,
             _price=None,
             _amount=None,
     ) -> str:
         pass
 
     @abc.abstractmethod
-    def create_sniffer(
+    def place_sniffer(
             self,
             _bind_id,
             _symbol,
@@ -201,7 +199,7 @@ class OrderBase:
         pass
 
     @abc.abstractmethod
-    def cancel_sniffer(self, _sniffer_id, _delay=None):
+    def cancel_sniffer(self, _order_id, _delay=None):
         pass
 
 
@@ -209,7 +207,7 @@ class StrategyBase:
     def __init__(self):
         self.order_mgr: Optional[OrderBase] = None
 
-    def update_on_event(self, _order_mgr):
+    def update_order_mgr(self, _order_mgr):
         self.order_mgr = _order_mgr
 
     @abc.abstractmethod
@@ -235,113 +233,3 @@ class StrategyBase:
     @abc.abstractmethod
     def on_stop(self, _event):
         pass
-
-
-class Order:
-    STATUS_DICT = {
-        'pending': 0,
-        'waiting': 1,
-        'triggered': 2,
-        'partial_filled': 3,
-        'canceling': 4,
-        'canceled': 5,
-        'error': 5,
-        'filled': 5
-    }
-
-    def __init__(
-            self,
-            _order_id,
-            _bind_id,
-            _symbol,
-            _order_type,
-            _action,
-            _position,
-            _feature=None,
-            _expire=None,
-            _delay=None,
-            _target_price=None,
-            _operator=None,
-            _condition_expire=None,
-            _price=None,
-            _amount=None
-    ):
-        self.order_id = _order_id
-        self.__bind_id = _bind_id
-        self.__symbol = _symbol
-        self.__order_type = _order_type
-        self.__action = _action
-        self.__position = _position
-        self.__feature = _feature
-        self.__strategy_id = None
-        self.__account_name = None
-        self.__expire = _expire
-        self.__delay = _delay
-        self.__target_price = _target_price
-        self.__operator = _operator
-        self.__condition_expire = _condition_expire
-        self.__price = _price
-        self.__amount = _amount
-        self.__execute_amount = None
-        self.__send_timestamp = None
-        self.__receive_timestamp = None
-        self.__status = 'pending'
-        self.__execute_price = None
-        self.__last_timestamp = None
-        self.__local_timestamp = None
-        self.__close_timestamp = None
-
-    def update_timestamp(self, _send_timestamp=None, _receive_timestamp=None):
-        if _send_timestamp:
-            self.__send_timestamp = _send_timestamp
-        if _receive_timestamp:
-            self.__receive_timestamp = _receive_timestamp
-
-    def update_account_name(self, _account_name):
-        self.__account_name = _account_name
-
-    def update_strategy_id(self, _strategy_id):
-        self.__strategy_id = _strategy_id
-
-    def update_status(self, _status, _last_timestamp, _local_timestamp, _execute_price, _execute_amount):
-        if self.STATUS_DICT[_status] < self.STATUS_DICT[self.__status]:
-            return
-
-        self.__status = _status
-        self.__last_timestamp = _last_timestamp
-        self.__local_timestamp = _local_timestamp
-        self.__execute_price = _execute_price
-        self.__execute_amount = _execute_amount
-        if self.STATUS_DICT[_status] >= 5:
-            self.__close_timestamp = _last_timestamp
-
-    @property
-    def order_info(self):
-        return {k.replace('__', ''): v for k, v in self.__dict__.items() if v is not None}
-
-
-class Sniffer:
-    def __init__(
-            self, _sniffer_id, _bind_id, _symbol, _operator, _target_price, _expire=None, _delay=None
-    ):
-        self.__sniffer_id = _sniffer_id
-        self.__bind_id = _bind_id
-        self.__symbol = _symbol
-        self.__operator = _operator
-        self.__target_price = _target_price
-        self.__expire = None
-        self.__delay = None
-        self.__status = 'pending'
-        self.__last_timestamp = None
-
-    def update_sniffer_feature(self, _expire=None, _delay=None):
-        self.__expire = _expire
-        self.__delay = _delay
-
-    def update_status(self, _status, _last_timestamp):
-        self.__status = _status
-        self.__last_timestamp = _last_timestamp
-
-    @property
-    def sniffer_info(self):
-        return {k.replace('__', ''): v for k, v in self.__dict__.items() if v is not None}
